d974bee57449dfd71ad660236a97c8485f88131e
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-17 13:33:34 +0700

    react router 6

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,6 +1,17 @@
 import Landing from "./pages/Landing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Landing, Error, Register } from "./pages";
 function App() {
-  return <Landing />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- fe/src/pages/Landing.js
+++ fe/src/pages/Landing.js
@@ -1,12 +1,12 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import styled from "styled-components";
-
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
@@ -23,7 +23,9 @@ export default function Landing() {
             yes plz sus humblebrag jianbing next level. Pour-over keffiyeh
             austin cronut, ugh occupy synth irony.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
