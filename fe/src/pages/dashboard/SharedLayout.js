4fb88a040a86da422244e2f9278a8568f07f7017
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-19 16:11:27 +0700

    shared layout

--- fe/src/pages/dashboard/SharedLayout.js
+++ fe/src/pages/dashboard/SharedLayout.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import styled from "styled-components";
+import { BigSidebar, Navbar, SmallSidebar } from "../../components";
 
 export default function SharedLayout() {
   return (
     <Wrapper>
-      <nav>
-        <Link to="all-jobs">all jobs</Link>
-        <Link to="add-job">add job</Link>
-      </nav>
-      <Outlet />
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
     </Wrapper>
   );
 }
