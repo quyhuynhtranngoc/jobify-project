f169d2c44b69a3ebbdf8f842a6f0fc9f50c547a1
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-21 21:45:35 +0700

    monthly applications fe

--- fe/src/components/ChartsContainer.js
+++ fe/src/components/ChartsContainer.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function ChartsContainer() {
-  return <div>ChartsContainer</div>;
-}
+import styled from "styled-components";
+import { useAppContext } from "../context/appContext";
+import AreaChartComponent from "./AreaChart";
+import BarChartComponent from "./BarChart";
+
+const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? (
+        <BarChartComponent data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
+  );
+};
+
+export default ChartsContainer;
+
+const Wrapper = styled.section`
+  margin-top: 4rem;
+  text-align: center;
+  button {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    color: var(--primary-500);
+    font-size: 1.25rem;
+    cursor: pointer;
+  }
+  h4 {
+    text-align: center;
+    margin-bottom: 0.75rem;
+  }
+`;
