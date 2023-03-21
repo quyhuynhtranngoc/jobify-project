4f9c625589ec10147128a2199c575e6309195d57
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-21 11:54:11 +0700

    get all jobs controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -13,7 +13,10 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-    res.send("update job");
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPage: 1 });
 };
 const updateJob = async (req, res) => {
   res.send("update job");
