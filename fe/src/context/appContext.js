e31a792f6f245b17fc3425d1e9e1db9bfefa9dfe
huynhtranngocquy2023 <huynhtranngocquy2023@gmail.com>
2023-03-21 08:42:51 +0700

    axios setup

--- fe/src/context/appContext.js
+++ fe/src/context/appContext.js
@@ -37,6 +37,39 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // axios
+  const authFetch = axios.create({
+    baseURL: "/api/v1",
+    // headers: {
+    //   Authorization: `Bearer ${state.token}`,
+    // },
+  });
+
+  //request
+  authFetch.interceptors.request.use(
+    (config) => {
+      // config.headers.common["Authorization"] = `Bearer ${state.token}`;
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  //response
+  authFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log("AUTH ERROR");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
     clearAlert();
@@ -133,9 +166,14 @@ const AppProvider = ({ children }) => {
   };
 
   const updateUser = async (currentUser) => {
-    console.log(currentUser);
+    try {
+      const { data } = await authFetch.patch("/auth/updateUser", currentUser);
+      console.log(data);
+    } catch (error) {
+      // console.log(error.response);
+    }
   };
-  
+
   return (
     <AppContext.Provider
       value={{
